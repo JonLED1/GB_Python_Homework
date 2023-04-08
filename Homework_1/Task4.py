305280481870ccc64e23d843d83243f7531aa75e
Jon LED <jonyl1977@gmail.com>
2023-04-08 15:07:15 +0300

    Готово задача 4 ДЗ1

---
+++ Homework_1/Task4.py
@@ -0,0 +1,5 @@
+summ_bird = int (input ("Введите количество журавликов: "))
+# уравнение x+x+(x+x)*2 = 6х
+serg_bird=petro_bird=summ_bird//6 
+cat_bird = (serg_bird+petro_bird)*2
+print (f"Сережа - {serg_bird}, Катя - {cat_bird}, Петя - {petro_bird}")
